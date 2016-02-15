02c05526b238b7303b44f18b400fd722967d752b
Sander <sander@plex.tv>
2016-02-15 18:25:46 +0100

    Add default icon; update show xpaths.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,16 +2,18 @@
 ESCAPIST_URL = 'http://www.escapistmagazine.com'
 ESCAPIST_GALERIES =  '%s/videos/galleries' % ESCAPIST_URL
 ESCAPIST_HIGHLIGHTS = '%s/ajax/videos_index.php?videos_type=%%s' % ESCAPIST_URL
+ICON = 'icon-default.jpg'
 
 ####################################################################################################
 def Start():
 
 	ObjectContainer.title1 = 'The Escapist'
+	DirectoryObject.thumb = R(ICON)
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0'
 
 ####################################################################################################
-@handler('/video/escapist', 'The Escapist')
+@handler('/video/escapist', 'The Escapist', thumb=ICON)
 def MainMenu():
 
 	oc = ObjectContainer()
@@ -26,20 +28,18 @@ def MainMenu():
 		title = 'Most Viewed'
 	))
 
-	for show in HTML.ElementFromURL(ESCAPIST_GALERIES).xpath('//div[@id="gallery_index_display"]/div[contains(@class, "folders_container")]'):
-		title = show.xpath('./a[@class="feedicon"]/@title')[0].split(' : ')[0]
-		url = show.xpath('./div[@class="folders_title_card"]/a/@href')[0].rstrip('/')
-		summary = show.xpath('./div[@class="article_links_container"]/p//text()')
-		summary = ''.join(summary)
+	for show in HTML.ElementFromURL(ESCAPIST_GALERIES).xpath('//a[@class="archive_container"]'):
 
-		try: thumb = show.xpath('.//a/img/@src')[0]
+		title = show.xpath('./text()')[0]
+		url = show.xpath('.//@href')[0]
+
+		try: thumb = 'http://%s' % (show.xpath('.//img/@src')[0].split('//')[-1])
 		except: thumb = ''
 
 		oc.add(DirectoryObject(
 			key = Callback(ShowBrowser, show_url=url, show_name=title, show_thumb=thumb),
 			title = title,
-			summary = summary,
-			thumb = Resource.ContentsOfURLWithFallback(thumb)
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)
 		))
 
 	return oc
@@ -58,6 +58,7 @@ def ShowBrowser(show_url, show_name, show_thumb, page_num=1):
 	html = HTML.ElementFromURL(page_url)
 
 	for episode in html.xpath('//div[@class="video"]//div[@id="gallery_display"]//div[@class="filmstrip_video"]'):
+
 		title = episode.xpath('.//div[@class="title"]//text()')[0]
 		date = episode.xpath('.//div[@class="date"]/text()')[0].strip('Date:').strip()
 		date = Datetime.ParseDate(date)
@@ -70,12 +71,13 @@ def ShowBrowser(show_url, show_name, show_thumb, page_num=1):
 		oc.add(VideoClipObject(
 			url = url,
 			title = title,
-			thumb = Resource.ContentsOfURLWithFallback(thumb),
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON),
 			originally_available_at = date
 		))
 
 	# Check for a next page link
 	if len(html.xpath('//a[@class="next_page"]')) > 0:
+
 		oc.add(NextPageObject(
 			key = Callback(ShowBrowser, show_url=show_url, show_name=show_name, show_thumb=show_thumb, page_num=page_num+1),
 			title = 'Next Page...'
@@ -91,6 +93,7 @@ def HighlightBrowser(title, mode):
 	html = HTML.ElementFromURL(ESCAPIST_HIGHLIGHTS % mode)
 
 	for episode in html.xpath('//div[@class="filmstrip_video"]'):
+
 		title = episode.xpath('.//div[@class="title"]//text()')[0]
 		date = episode.xpath('.//div[@class="date"]/text()')[0].strip('Date:').strip()
 		date = Datetime.ParseDate(date)
@@ -103,7 +106,7 @@ def HighlightBrowser(title, mode):
 		oc.add(VideoClipObject(
 			url = url,
 			title = title,
-			thumb = Resource.ContentsOfURLWithFallback(thumb),
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON),
 			originally_available_at = date
 		))
 
